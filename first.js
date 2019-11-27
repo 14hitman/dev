f3481d5cb2059f6e4c92b61f1cc03b8ec24bc0e7
14hitman <rohitmailfrcareer@gmail.com>
2019-11-27 17:46:21 +0530

    first 2 file added

--- first.js
+++ first.js
@@ -5,3 +5,10 @@ ngOnInit() {
     this.emps = result;
   });
 }
+ngOnInit() {
+    let observableResult=  this.service.Select();
+    observableResult.subscribe((result)=>{
+    console.log(result);
+    this.emps = result;
+  });
+}
